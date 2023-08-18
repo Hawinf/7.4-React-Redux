5c6666e3c16520be07a406b887f7a1255eda541a
Hawinf <hawinf54@gmail.com>
2023-08-18 22:42:19 +0400

    calling regisReducer value to regis page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,12 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useSelector } from "react-redux";
+import rootReducer from "../redux/reducers";
 
 const Register = () => {
+    const {regisReducer} = useSelector(rootReducer => rootReducer);
+    console.log(regisReducer.message)
+
     return (
         <div>
             <Navbar />
