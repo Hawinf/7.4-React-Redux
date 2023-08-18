b46fa7b3aba8f93f16990face5fa96aadb130da2
Hawinf <hawinf54@gmail.com>
2023-08-18 22:58:50 +0400

    setting up register page and adding const folder endpoint.js

---
+++ src/const/endpoint.js
@@ -0,0 +1,5 @@
+const API = {
+    REGISTER: 'https://bootcamp-rent-cars.herokuapp.com/admin/auth/register',
+}
+
+export default API;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../components/Navbar";
 import { useSelector } from "react-redux";
 import rootReducer from "../redux/reducers";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import API from "../const/endpoint";
 
 const Register = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
     const {regisReducer} = useSelector(rootReducer => rootReducer);
     console.log(regisReducer.message)
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleRegister = () => {
+        const payload = {
+            email: email,
+            password: password,
+            role: 'Admin',
+        }
+        axios
+            .post(API.REGISTER, payload)
+            .then((res) => {
+                console.log(res)
+            })
+            .catch((err) => console.log(err))
+    }
 
     return (
         <div>
             <Navbar />
             <h1>Register</h1>
+            <input placeholder="Email" onChange={handleEmail} />
+            <input placeholder="Password" onChange={handlePassword} />
+            <button onClick={handleRegister}>Register</button>
         </div>
     );
 };
